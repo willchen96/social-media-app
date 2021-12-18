7cdc9420d806c4e681f8d9bd1aa7d9230fcd339f
Will <weiye.chen96@icloud.com>
2021-12-19 02:43:13 +0800

    for deployment to heroku via cli 3

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const router = require("./router")
+const path = require("path")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
@@ -39,7 +40,7 @@ app.use((req, res, next) => {
   res.locals.user = req.session.user
   next()
 })
-app.set("views", "views")
+app.set("views", path.join(__dirname, "/views"))
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 
